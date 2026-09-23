a710d6f3407bc8e7e0cb3bf1b8d73262a2c2c3c2
maryam22001 <maryamosman0011@gmail.com>
2026-09-23 18:23:20 +0300

    fix app.tsx and config file

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-import { useState } from 'react';
-import type { ComponentType } from 'react';
+import { useState, type ComponentType } from 'react';
 import * as IsoflowModule from 'isoflow';
 import { Architecture2D } from './Architecture2D';
 import { CleanArchitectureView } from './CleanArchitectureView';
